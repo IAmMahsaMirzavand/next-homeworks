021b72018cefc7e49310731d27ed735f70f676db
IAmMahsaMirzavand <mahsacb74@gmail.com>
2024-12-23 08:58:14 +0330

    add header

--- nextjs-1-homework/my-app/src/app/layout.js
+++ nextjs-1-homework/my-app/src/app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body  style={{ backgroundColor: '#bbdefb' }}>
-        {/* <Header/> */}
+         <Header/>
         {children}
       </body>
     </html>
